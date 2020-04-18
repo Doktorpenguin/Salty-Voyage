de89ebd526732d169743c971ee312e780d1588f5
Joel <nintendogamer249@gmail.com>
2020-04-18 00:36:54 -0400

    Movement Done
    The "Captain" can move and collide with the edges of the place holder ship.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    //This script handles the Character's movements and interactions with the world.
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    
+    Vector2 movement;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
